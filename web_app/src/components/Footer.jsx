e215324ee3050225318ae8caa128a2617339fc90
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-07-15 17:37:13 +0545

    added footer

---
+++ web_app/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+// src/components/Footer.jsx
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className=" bg-gray-100 text-black p-4 mt-8 items-baseline">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+          © 2024 House Price Predictor. All rights reserved.
+        </p>
+        <p className="text-sm">Designed by Raj Kumar</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
